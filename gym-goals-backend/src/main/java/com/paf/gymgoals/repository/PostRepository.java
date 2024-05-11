44a73f51109891e8b925443ee246cab3c9192682
21213908it <harshaviraj2001@gmail.com>
2024-05-11 23:31:22 +0530

    Comment_CRUD

--- gym-goals-backend/src/main/java/com/paf/gymgoals/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.paf.gymgoals.repository;
-
-import com.paf.gymgoals.model.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends MongoRepository<Post, String> {
-}
--- gym-goals-backend/src/main/java/com/paf/gymgoals/service/LikeService.java
+++
@@ -1,9 +0,0 @@
-package com.paf.gymgoals.service;
-
-import com.paf.gymgoals.model.Like;
-
-public interface LikeService {
-    void addLike(Like like);
-
-    void removeLikeById(String likeId);
-}
